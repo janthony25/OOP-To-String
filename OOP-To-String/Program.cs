193750c7f1d0449817d2c959d46947b2eaf708ae
janthony25 <junanthony.business@gmail.com>
2024-02-04 11:28:32 +1300

    Add project files.

---
+++ OOP-To-String/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // ToStrign() = converts and object to its strign representation so that it is suitable for display
+
+            Car car = new Car("Chevy", "Corvette", 2022, "blue");
+
+            Console.WriteLine(car);
+
+            Console.ReadKey();
+        }
+    }
+
+    class Car
+    {
+        string make;
+        string model;
+        int year;
+        string color;
+
+        public Car(string make, string model, int year, string color)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.color = color;
+        }
+        public override string ToString()
+        {
+            string message = $"This car is a {make} {model}";
+            return message;
+        }
+
+    }
+}
